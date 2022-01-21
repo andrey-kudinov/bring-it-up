a8557f230cf984b6640211c5737f4c0e7dbe7251
Andrey Kudinov <2499931@gmail.com>
2022-01-21 23:57:20 +0400

    add args

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import { Slider } from "./modules/index"
 
 window.addEventListener('DOMContentLoaded', () => {
-  const slider = new Slider('.page')
+  const slider = new Slider('.page', '.next')
   slider.render()
 })
