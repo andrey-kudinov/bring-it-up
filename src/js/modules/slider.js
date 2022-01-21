af28780df8712fb1bb030334fe0f4c3306fbf7d7
Andrey Kudinov <2499931@gmail.com>
2022-01-21 23:57:35 +0400

    add methods to slider

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -3,9 +3,45 @@ export class Slider {
     this.page = document.querySelector(page)
     this.slides = this.page.children
     this.buttons = document.querySelectorAll(buttons)
+    this.slideIndex = 1
   }
 
+  showSlides(n) {
+    if (n > this.slides.length) {
+      this.slideIndex = 1
+    }
+
+    if (n < 1) {
+      this.slideIndex = this.slides.length
+    }
+
+    console.log(this.slides)
+
+    this.slides.forEach(slide => {
+      slide.style.display = 'none'
+    })
+
+    this.slides[this.slideIndex - 1].style.display = 'block'
+  }
+
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n)
+  }
+
+
   render() {
-    console.log(this.page, this.slides)
+    this.buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        this.plusSlides(1)
+      })
+
+      button.parentNode.previousElementSibling.addEventListener('click', (e) => {
+        e.preventDefault()
+        this.slideIndex = 1
+        this.showSlides(this.slideIndex)
+      })
+    })
+
+    this.showSlides(this.slideIndex)
   }
 }
