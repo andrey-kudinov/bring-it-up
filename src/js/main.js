b1e1f1f6f7d8426f69dc3153575aab679ea1af88
Andrey Kudinov <2499931@gmail.com>
2022-01-25 23:58:09 +0400

    add mini sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,38 @@
-import { Video } from "./modules/index"
-import { MainSlider } from "./modules/slider/slider-main"
+import { Video, MainSlider, MiniSlider } from "./modules/index"
 
 window.addEventListener('DOMContentLoaded', () => {
-  const mainSliderArgs = {
-    page: '.page',
+  const slider = new MainSlider({
+    container: '.page',
     buttons: '.next'
-  }
-  const slider = new MainSlider(mainSliderArgs)
+  })
   slider.render()
+
+  const showUpSlider = new MiniSlider({
+    container: '.showup__content-slider',
+    prev: '.showup__prev',
+    next: '.showup__next',
+    activeClass: 'card-active',
+    animate: true
+  })
+  showUpSlider.init()
+
+  const modulesSlider = new MiniSlider({
+    container: '.modules__content-slider',
+    prev: '.modules__info-btns .slick-prev',
+    next: '.modules__info-btns .slick-next',
+    activeClass: 'card-active',
+    animate: true,
+    autoplay: true
+  })
+  modulesSlider.init()
+
+  const feedSlider = new MiniSlider({
+    container: '.feed__slider',
+    prev: '.feed .slick-prev',
+    next: '.feed .slick-next',
+    activeClass: 'feed__item-active'
+  })
+  feedSlider.init()
 
   const video = new Video('.showup .play', '.overlay')
   video.init()
